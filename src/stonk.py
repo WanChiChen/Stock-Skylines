c310aa294f8d2c70148834e1e7b783ec6ab15dec
William Chen <wchen6255@Knights.ucf.edu>
2020-12-19 15:53:39 -0500

    only keep closing date

--- src/stonk.py
+++ src/stonk.py
@@ -3,7 +3,8 @@ import yfinance as yf
 def get_data(ticker, period, start, end):
     tickerData = yf.Ticker(ticker)
     tickerDf = tickerData.history(period=period, start=start, end=end)
-
+    tickerDf = tickerDf[['Close']]
     print(tickerDf)
+    
 
 get_data('MSFT', '1d', '2010-01-01', '2020-01-25')
