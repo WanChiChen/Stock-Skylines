b33ea1f16eaa1f961bb5e35bbbcdf624b093146d
William Chen <wchen6255@Knights.ucf.edu>
2020-12-19 15:43:58 -0500

    graph chart

--- src/graph.py
+++ src/graph.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 from PIL import Image, ImageOps
 from otsu import run
 
@@ -8,14 +10,19 @@ def graph(src):
     img = np.asarray(img)
     rows, cols = img.shape
     graph_img = np.zeros((rows, cols))
+    graph = np.zeros(cols)
 
     for j in np.arange(cols):
         for i in np.arange(rows):
             if img[i, j] == 0:
                 graph_img[i, j] = 255
+                graph[j] = rows - i
                 break
-    img = Image.fromarray(graph_img)
-    img.convert("L").save(f"../graphs/{src}")
 
-run("shanghai.jpg")
+    plt.plot(np.arange(cols), graph)
+    plt.ylim(0, 700)
+    img = Image.fromarray(graph_img)
+    img.convert("L").save(f"../graphs/images/{src}")
+    plt.savefig(f"../graphs/charts/{src}")
+
 graph("shanghai.jpg")
