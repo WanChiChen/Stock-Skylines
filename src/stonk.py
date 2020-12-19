2198a341a324184f85ecb6d7c92787f57f2be4a2
William Chen <wchen6255@Knights.ucf.edu>
2020-12-19 15:49:53 -0500

    stock data

---
+++ src/stonk.py
@@ -0,0 +1,9 @@
+import yfinance as yf
+
+def get_data(ticker, period, start, end):
+    tickerData = yf.Ticker(ticker)
+    tickerDf = tickerData.history(period=period, start=start, end=end)
+
+    print(tickerDf)
+
+get_data('MSFT', '1d', '2010-01-01', '2020-01-25')
