fix scaling

src/graph.py
@@ -42,13 +42,14 @@ def graph_stonk(city, ticker, period, start, end):
 
     if length_scale > 1:
         length_scale = np.ceil(length_scale)
-        data = data[data.index % length_scale != 0]
+        print(length_scale)
+        data = data[data.index % length_scale == 0]
         data = data.reset_index()
         data = data.drop('index', axis=1)
 
     if length_scale < 1:
         length_scale = ( 1 / length_scale)
-        length_Scale = int(np.ceil(length_scale))
+        length_scale = int(np.ceil(length_scale))
         indicies = np.arange(0, city_graph.size, length_scale, dtype='int64')
         city_graph = np.take(city_graph, indicies)
 
@@ -65,4 +66,4 @@ def graph_stonk(city, ticker, period, start, end):
     data.plot()
     plt.show()
 
-graph_stonk('shanghai', 'M', '1d', '2010-01-01', '2020-12-16')
+graph_stonk('chicago', 'JPM', '1d', '2010-01-01', '2020-12-16')